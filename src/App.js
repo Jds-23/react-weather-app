2c7014f680442c14bc76a2226011733e62d10156
joydeep singha <joydeepsingha68@gmail.com>
2021-02-16 19:01:25 +0530

    Added install button in the header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import GetForecast from "./Components/GetForecast";
-import {Container,Content,Footer,FlexboxGrid,Divider} from "rsuite";
+import {Container,Content,Footer,FlexboxGrid,Divider,Button} from "rsuite";
 import MyFooter from "./Components/Footer";
 
 function App() {
+    const [installable,setInstallable]=useState(false);
+
+    window.addEventListener('beforeinstallprompt', (event) => {
+        window.deferredPrompt = event;
+        setInstallable(true)
+      });
+
+      const handleInstall=()=>{
+        const promptEvent = window.deferredPrompt;
+        if (!promptEvent) {
+          // The deferred prompt isn't available.
+          return;
+        }
+        // Show the install prompt.
+        promptEvent.prompt();
+        // Log the result
+        promptEvent.userChoice.then((result) => {
+          // Reset the deferred prompt variable, since
+          // prompt() can only be called once.
+          window.deferredPrompt = null;
+          // Hide the install button.
+         setInstallable(false)
+        });
+
+      }
     return (
         <div className="App">
+            {installable?<div style={{borderBottom:"1px lightgray solid", display:"flex",justifyContent:"flex-end",padding:"2px 5px"}}>
+                <Button appearance="primary" size="sm" onClick={handleInstall}>Install</Button>
+            </div>:<></>}
             <Container>
                 <Content>
                     <FlexboxGrid justify="center">
